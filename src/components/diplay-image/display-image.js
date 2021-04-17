2c95d897a9c1dd7c6a95ea041eb3683314194ab6
raphaelbmesquita123 <raphaelbmesquita@gmail.com>
2021-04-17 15:10:51 +0100

    our-history and display-image updated

--- src/components/diplay-image/display-image.js
+++ src/components/diplay-image/display-image.js
@@ -1,33 +1,10 @@
-import React, { useState }from 'react'
+import React from 'react'
 import './display-image.style.css'
-import IMAGE_DATA from './display-image-data/diplay-image-data'
-
-function DisplayImage () {
-
-    const [nextPhoto, setNextPhoto] = useState(0)
-    const goLeftPhoto = () => setNextPhoto(nextPhoto + 100)
-    const goRightPhoto = () => setNextPhoto(nextPhoto - 100)
 
 
+function DisplayImage ({ src, alt }) {
     return(
-        <div className="displayImage">
-            <div className="imageData" style={{ transform: `translateX(${nextPhoto}%)` }}>
-                <img src={IMAGE_DATA[2].imageUrl} alt="nada" />  
-                <img src={IMAGE_DATA[2].imageUrl} alt="nada" />    
-                <img src={IMAGE_DATA[2].imageUrl} alt="nada" />    
-            </div>
-            
-            <div className="imageText">
-                <div>
-                    <h3>{IMAGE_DATA[0].year}</h3>
-                    <small>{IMAGE_DATA[0].text}</small>
-                </div>    
-                  
-            </div>
-                <i className="fas fa-chevron-left" onClick={ goLeftPhoto }/>
-                <i className="fas fa-chevron-right" onClick={ goRightPhoto }/>
-        </div>
-        
+        <img className="diplayImage" src={ src } alt ={ alt }/>        
     )
 }
 
--- src/containers/our-history/our-history.container.js
+++ src/containers/our-history/our-history.container.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DisplayImage from '../../components/diplay-image/display-image'
+import IMAGE_DATA from '../../data/image-data/diplay-image-data'
 import './our-history.style.css'
 
 
 function OurHistory () {
+    const [nextPhoto, setNextPhoto] = useState(0)
+    const goLeftPhoto = () => setNextPhoto(nextPhoto + 100)
+    const goRightPhoto = () => setNextPhoto(nextPhoto - 100)
+
     return(
         <div className="ourHistory" id="ourHistory">
+            <div className="skewedTop">
+            </div>
             <h2>Nossa Historia</h2>
             <div className="ourHistoryContent">
-                <div>
-                    <DisplayImage />
+                <div style={{ transform: `translateX(${nextPhoto}%)`}}>
+                    <DisplayImage src={IMAGE_DATA[1].imageUrl} alt={IMAGE_DATA[2].alt}/>
+                    <DisplayImage src={IMAGE_DATA[2].imageUrl} alt={IMAGE_DATA[2].alt}/>
+                    <DisplayImage src={IMAGE_DATA[0].imageUrl} alt=
+                    {IMAGE_DATA[2].alt}/>
                 </div>
+                <i className="fas fa-chevron-left" onClick={ goLeftPhoto }/>
+                <i className="fas fa-chevron-right" onClick= { goRightPhoto } />
             </div>
-
-            <div className="skewed">
-                
+            <div className="skewedBotton">
             </div>
         </div>    
     )
